76d830577ad597989bb82240cc84f1735a63bdd8
shakthi <shakthikumar123456789@gmail.com>
2022-10-21 15:19:53 +0530

    Create weather card component

---
+++ src/compoents/weatherCard.jsx
@@ -0,0 +1,16 @@
+const WeatherCard = ({ when, status, min, max }) => {
+  return (
+    <div className="weather-card">
+      <div className="when">{when}</div>
+
+      <img className="weather-icon" src={`/images/${status}.png`} alt={when} />
+
+      <div className="tempertures">
+        <span>{max}</span>
+        <span>{min}</span>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherCard;
--- src/section/right.jsx
+++ src/section/right.jsx
@@ -1,5 +1,11 @@
+import WeatherCard from "./../compoents/weatherCard";
+
 const RightSide = () => {
-  return <div className="right"></div>;
+  return (
+    <div className="right">
+      <WeatherCard when="today" status="HeavyRain" min="11°C" max="16°C" />
+    </div>
+  );
 };
 
 export default RightSide;
